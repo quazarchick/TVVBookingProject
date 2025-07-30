0dfd1ce721069561235d7a8744f0ad4ce3048def
VladimirPC <vteplakov@gmail.com>
2025-07-30 19:07:07 +0300

    Добавил тесты create booking with custom and random data

--- conftest.py
+++ conftest.py
@@ -16,9 +16,10 @@ def booking_dates():
     checkout_date = checkin_date + timedelta(days=5)
     return  {
         "checkin": checkin_date.strftime('%Y-%m-%d'),
-        "checkuot": checkout_date.strftime('%Y-%m-%d')
+        "checkout": checkout_date.strftime('%Y-%m-%d')
     }
 
+@pytest.fixture
 def generate_random_booking_data(booking_dates):
     faker = Faker()
     firstname = faker.first_name()
